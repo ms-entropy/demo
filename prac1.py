5d70e8b1f687b5dc1c2432eef8807fcbcc9284b0
ms-entropy <75878956+ms-entropy@users.noreply.github.com>
2021-06-12 12:58:39 +0530

    Add files via upload

---
+++ prac1.py
@@ -0,0 +1,11 @@
+hi = "hello"
+name ="proff"
+greet = hi+name
+print(greet)
+print(hi+" "+name)
+print(hi + (" "+name)*3)
+print(hi + (3*(" "+name)))
+a=int(input("\n typre first element: "))
+b=int(input("\n type 2 element : "))
+c=a+b
+print("sum of elements : ",c)
